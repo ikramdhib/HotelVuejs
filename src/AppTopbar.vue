13070448f37ac8d37622740849f58a9573482900
ikramdhib <dhibikram50@gmail.com>
2022-03-18 17:40:53 +0100

    admin profile

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -23,6 +23,26 @@
 			<li>
 				<button class="p-link layout-topbar-button">
 					<i class="pi pi-cog"></i>
+					 <select class="form-select appearance-none
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding bg-no-repeat
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+        <option>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+					 </select>
 					<span>Settings</span>
 				</button>
 			</li>
