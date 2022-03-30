911bdbab07976c7a3f2c31b55aa8a340d6409e76
ikramdhib <dhibikram50@gmail.com>
2022-03-30 12:41:43 +0100

    logout && dropdownprofile

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -21,16 +21,40 @@
 				</button>
 			</li>
 			<li>
-				<button class="p-link layout-topbar-button" v-on:click="logout()">
-					<i class="pi pi-fw pi-sign-out"></i>
-					<span>Quitter</span>
-				</button>
-			</li>
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-user"></i>
-					<span>Profile</span>
-				</button>
+				<div class="ml-3 relative">
+  <div v-on:click="isActive = !isActive">
+    <button
+      class="flex bg-white text-sm border-2 p-link layout-topbar-button border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out"
+      id="user-menu"
+      aria-label="User menu"
+      aria-haspopup="true"
+    >
+    <i class="pi pi-user"></i>
+    </button>
+  </div>
+    <div v-show="isActive" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+    <div
+      class="py-1 w-12 rounded-md bg-white shadow-xs"
+      role="menu"
+      aria-orientation="vertical"
+      aria-labelledby="user-menu"
+    >
+      <router-link
+	  to="profile"
+        href="#"
+        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+        role="menuitem"
+      > Profile</router-link>
+      
+      <a
+        href="#"
+        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+        role="menuitem"
+		v-on:click="logout()"
+      >Quiter</a>
+    </div>
+  </div>
+</div>
 			</li>
 		</ul>
 	</div>
@@ -39,7 +63,11 @@
 <script>
 import axios from 'axios';
 export default {
-
+data: function () {
+        return {
+            isActive: false,
+        }
+    },
     methods: {
         onMenuToggle(event) {
             this.$emit('menu-toggle', event);
