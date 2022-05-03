8c5e730f51b3388f28b2107e39fe5d334115ee32
ikramdhib <dhibikram50@gmail.com>
2022-05-03 21:38:12 +0100

    menu update

--- src/App.vue
+++ src/App.vue
@@ -36,92 +36,110 @@ export default {
             mobileMenuActive: false,
             menu : [
                 {
-                    label: 'Home',
+                    label: 'Accueil',
                     items: [{
                         label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/'
                     }]
                 },
-				{
-					label: 'UI Components', icon: 'pi pi-fw pi-sitemap',
-					items: [
-                        {label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/formlayout'},
-						{label: 'Calendrier', icon: 'pi pi-fw pi-calendar', to: '/calendrier'},
-						{label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/input'},
-                        {label: "Float Label", icon: "pi pi-fw pi-bookmark", to: "/floatlabel"},
-                        {label: "Invalid State", icon: "pi pi-fw pi-exclamation-circle", to: "/invalidstate"},
-						{label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/button'},
-						{label: 'Table', icon: 'pi pi-fw pi-table', to: '/table'},
-						{label: 'List', icon: 'pi pi-fw pi-list', to: '/list'},
-						{label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/tree'},
-						{label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/panel'},
-						{label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/overlay'},
-                        {label: "Media", icon: "pi pi-fw pi-image", to: "/media"},
-						{label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/menu'},
-						{label: 'Message', icon: 'pi pi-fw pi-comment', to: '/messages'},
-						{label: 'File', icon: 'pi pi-fw pi-file', to: '/file'},
-						{label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/chart'},
-						{label: 'Misc', icon: 'pi pi-fw pi-circle-off', to: '/misc'},
-                         {label: 'Option', icon: 'pi pi-fw pi-circle-off', to: '/option'},
-					]
-				},
                 {
-                    label: 'PrimeBlocks',
-                    items: [
-                        {label: 'Free Blocks', icon: 'pi pi-fw pi-eye', to: '/blocks', badge: "NEW"},
-                    ]
-                },
-                {
-                    label: 'Utilities',
-                    items: [
-                        {label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/icons'},
-                    ]
-                },
-				{
-					label: 'Pages', icon: 'pi pi-fw pi-clone',
-					items: [
-						{label: 'Crud', icon: 'pi pi-fw pi-user-edit', to: '/crud'},
-						{label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/timeline'},
-                        {label: 'Landing', icon: 'pi pi-fw pi-globe', to: '/landing'},
-                        {label: 'Error', icon: 'pi pi-fw pi-times-circle', to: '/error'},
-                        {label: 'Profile', icon: 'pi pi-fw pi-times-circle', to: '/profile'},
-                        {label: 'Not Found', icon: 'pi pi-fw pi-exclamation-circle', to: '/notfound'},
-                        {label: 'Access Denied', icon: 'pi pi-fw pi-lock', to: '/access'},
-                       
-					]
-				},
-                {
-                    label: 'Menu Hierarchy', icon: 'pi pi-fw pi-search',
+                    label: 'Hôtellerie', icon: 'pi pi-fw pi-search',
                     items: [
                         
                         {
-                            label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
+                            label: 'Chambres',
                             items: [
                                 {
-                                    label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 2.1.3', icon: 'pi pi-fw pi-bookmark'},
-                                    ]
+                                    label: 'Ajouter un chambre', icon: 'pi pi-plus',
+                                    to:'AddRoom'
                                 },
                                 {
-                                    label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
-                                    items: [
-                                        {label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'},
-                                        {label: 'Submenu 2.2.2', icon: 'pi pi-fw pi-bookmark'}
-                                    ]
+                                    label: 'Les chambres', icon: 'pi pi-list', 
+                                    to:'TableRoom'
+                                }
+                            ]
+                        },
+                         {
+                            label: 'Salle de Conference',
+                            items: [
+                                {
+                                    label: 'Ajouter une Salle', icon: 'pi pi-plus',
+                                    to:'addconferenceroom'
+                                },
+                                {
+                                    label: 'Les salles de cinference', icon: 'pi pi-list',
+                                    to:'conferencerooms'
+                                },
+                            ]
+                        },
+                         {
+                            label: 'Resataurants', 
+                            items: [
+                                {
+                                    label: 'Ajouter un restaurant ', icon: 'pi pi-plus',
+                                    to:'/addrestaurant'
+                                },
+                                {
+                                    label: 'Les Restaurants', icon: 'pi pi-list',
+                                    to:'restaurants'
+                                }
+                            ]
+                        },
+                        {
+                            label: 'SPAs', 
+                            items: [
+                                {
+                                    label: 'Ajouter une SPA ', icon: 'pi pi-plus',
+                                    to:'spa'
+                                },
+                                {
+                                    label: 'Les SPAs', icon: 'pi pi-list',
+                                    to:''
+                                }
+                            ]
+                        },
+                        {
+                            label: 'Pools', 
+                            items: [
+                                {
+                                    label: 'Ajouter un Pool ', icon: 'pi pi-plus',
+                                    to:'pool'
+                                },
+                                {
+                                    label: 'Les Pools', icon: 'pi pi-list',
+                                    to:''
+                                }
+                            ]
+                        },
+                        {
+                            label: 'Roof-Top', 
+                            items: [
+                                {
+                                    label: 'Ajouter un Roof-Top ', icon: 'pi pi-plus',
+                                    to:'/addrooftop'
+                                },
+                                {
+                                    label: 'Les Roof-Top', icon: 'pi pi-list',
+                                    to:'rooftop'
+                                }
+                            ]
+                        },
+                         {
+                            label: 'Roof-Top', 
+                            items: [
+                                {
+                                    label: 'Ajouter un Roof-Top ', icon: 'pi pi-plus',
+                                    to:'/addrooftop'
+                                },
+                                {
+                                    label: 'Les Roof-Top', icon: 'pi pi-list',
+                                    to:'rooftop'
                                 }
                             ]
                         }
                     ]
                 },
-                {
-                    label: 'Get Started', 
-                    items: [
-                        {label: 'Documentation', icon: 'pi pi-fw pi-question', command: () => {window.location = "#/documentation"}},
-                        {label: 'View Source', icon: 'pi pi-fw pi-search', command: () => {window.location = "https://github.com/primefaces/sakai-vue"}}
-                    ]
-                }
+                
+               
             ]
         }
     },
